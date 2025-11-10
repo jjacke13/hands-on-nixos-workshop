1a72cb561be806bcb9efe6432d1318d629f7b3c1
Vaios K <jjacke@proton.me>
2025-11-10 20:37:50 +0200

    gitea module

--- nixos-files/gitea.nix
+++ nixos-files/gitea.nix
@@ -5,7 +5,7 @@
     services.gitea.dump.enable = false;
     services.gitea.settings.log.LEVEL = "Critical";
     services.gitea.settings.server.HTTP_ADDR = "127.0.0.1";
-    services.gitea.settings.server.ROOT_URL = "%(PROTOCOL)s://%(DOMAIN)s:%(HTTP_PORT)s/gitea/";
+    services.gitea.settings.server.ROOT_URL = "%(PROTOCOL)s://%(DOMAIN)s:%(HTTP_PORT)s";
     services.gitea.settings.server.HTTP_PORT = 3000;
 
 }
