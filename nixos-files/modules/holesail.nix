9fe218889dcc35eac24029eeeff60ed28dd5ed2e
Vaios K <jjacke@proton.me>
2025-11-10 20:45:18 +0200

    holesail module

---
+++ nixos-files/modules/holesail.nix
@@ -0,0 +1,11 @@
+{
+  #### Holesail to expose Nextcloud! ####
+  services.holesail-server.holesail-nextcloud = {
+  	enable = true;
+  	host = "127.0.0.1";
+    port = 80;
+    connector = "<connection-string-here>";
+  };
+  #######################################
+
+}
